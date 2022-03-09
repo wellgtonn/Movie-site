aff7179e319c5f38d975f241fcbc44f7608a52c0
Welington Junior <welingtonfsjbh@gmail.com>
2022-03-09 18:43:58 -0300

    Projeto criado

---
+++ backend/src/test/java/welington/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package welington.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
