eb3563d4ad964bc1a8b1c6e7a2a033962106e6f4
Welington Junior <welingtonfsjbh@gmail.com>
2022-03-09 22:42:42 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,35 @@
-import Pagination from "componentes/Pagination";
+import MovieCard from 'componentes/MovieCard'
+import MovieScore from 'componentes/MovieScore'
+import MovieStars from 'componentes/MovieStars'
+import Pagination from 'componentes/Pagination'
 function Listing() {
+  return (
+    <>
+      <Pagination />
 
-  return(
-    <Pagination/>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
-export default Listing;
+export default Listing
