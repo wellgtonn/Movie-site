e5c3be935a40530a870fd73e7388bd72d4b43271
Welington Junior <welingtonfsjbh@gmail.com>
2022-03-12 09:58:37 -0300

    Erro do postgre

--- backend/src/main/java/welington/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/welington/dsmovie/DsmovieApplication.java
@@ -2,8 +2,9 @@ package welington.dsmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-
+@EntityScan(basePackages =" welington.dsmovie.entities")
 @SpringBootApplication
 public class DsmovieApplication {
 
